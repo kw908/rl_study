dc5f47c3ab9efdabce020d1d9da9ff2fd6c7a16a
Kai Wang <20kw16@queensu.ca>
2023-10-24 22:34:33 -0400

    finished evaluating random policy for grid world in Example 4.1

--- Environments/gridworld_v0.py
+++ Environments/gridworld_v0.py
@@ -3,17 +3,17 @@
 ## use tuples to denote states (grid positions)
 import math
 import numpy as np
-from copy import deepcopy
+from copy import copy
 
 class environment():
-  def __init__(self,sidelength) -> None:
+  def __init__(self,sidelength,episode) -> None:
     self.sidelength = sidelength
     self.l = sidelength-1
-    self.action = ["up","down","left","right"]
+    self.actions = ["up","down","left","right"]
     self.states = []
     for i in range(self.sidelength):
        for j in range(self.sidelength):
-          self.states.append(state([i,j]))
+          self.states.append(state([i,j],episode))
     
     self.terminal_states = [[0,0],[self.l,self.l]]
   
@@ -31,37 +31,44 @@ class environment():
 #     self.agent_state = start_state
     
 
-  def step(self,state,action):
+  def step(self,state,action,episode):
 
-    next_state = deepcopy(state)
+    next_state_alias = copy(state.alias)
+    next_state_value = copy(state.value[episode])
     reward = 0
 
+    if state.alias in self.terminal_states:
+       reward = 0
+       next_state_value = 0
+       return next_state_alias, next_state_value, reward
+
     if action == "up":
-        next_state.alias[0] -= 1
+        next_state_alias[0] -= 1
     if action == "down":
-        next_state.alias[0] += 1
+        next_state_alias[0] += 1
     if action == "left":
-        next_state.alias[1] -= 1
+        next_state_alias[1] -= 1
     if action == "right":
-        next_state.alias[1] += 1
+        next_state_alias[1] += 1
     
-    if next_state.alias in self.terminal_states:
+    if next_state_alias in self.terminal_states:
       reward = -1 
-      next_state.value = 0
-      return next_state, reward
+      next_state_value = 0
+      return next_state_alias, next_state_value, reward
     
     else:
-        for pos in next_state.alias:
+        for pos in next_state_alias:
             if pos < 0 or pos > self.l: #off the grid case
                 reward = -1
-                next_state.alias = state.alias
-                next_state.value = state.value
-                return next_state, reward
+                next_state_alias = copy(state.alias)
+                next_state_value = copy(state.value[episode])
+                return next_state_alias, next_state_value, reward
             else: 
                 reward = -1
-                i = self.pair_to_number(next_state.alias)
-                next_state.value = self.state[i]
-                return next_state, reward
+                i = self.pair_to_number(next_state_alias)
+                next_state_alias = self.states[i].alias
+                next_state_value = self.states[i].value[episode]
+                return next_state_alias, next_state_value, reward
     
 
   def render(self):
@@ -72,6 +79,6 @@ class agent():
       self.state = state([0,0])
 
 class state():
-   def __init__(self,position:[int,int]) -> None:
+   def __init__(self,position:[int,int],episode:int) -> None:
       self.alias = position
-      self.value = 0
+      self.value = np.zeros(episode+1)
