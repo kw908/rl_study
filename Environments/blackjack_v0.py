5faf0e8cdfaa02e44362e16eb63fd106f76092f3
Kai Wang <20kw16@queensu.ca>
2023-10-29 03:09:54 -0400

    changed file name, still working on first visit MC

--- Environments/blackjack_v0.py
+++ Environments/blackjack_v0.py
@@ -3,6 +3,7 @@
 import math
 import numpy as np
 import random
+import itertools as iter
 
 """for rendering"""
 import matplotlib.pyplot as plt
@@ -22,38 +23,91 @@ class environment():
         
         self.dealer_cards = []
         self.player_cards = []
-        self.reward = 0
+
+        self.step = 0
+
+        r = [-1, 0, 1]
+        dealer_showing = [1,2,3,4,5,6,7,8,9,10]
+        player_sum = [12,13,14,15,16,17,18,19,20,21]
+        self.states = list(iter.product(r, dealer_showing, player_sum)) #200 states total
 
     def reset(self):
         self.dealer_cards = []
         self.player_cards = []
-        self.reward = 0
 
     def hits(self): #draw a card
         random.seed()
         i = random.randint((1,52))
+        self.step+=1
         return self.deck[i]
 
     
     def dealer(self):
         s = sum(self.dealer_cards)
-        if s>=17: 
-            return "sticks"
-        else:
+        if s>=17 and s<=21: 
+            return s, "sticks"
+        elif s<17:
             self.dealer_cards.append(self.hits())
             return "hits"
+        else: return "busted"
     
     def agent(self): #player with policy
-        s = sum(self.dealer_cards)
-        if s>=20: 
-            return "sticks"
-        else:
-            self.dealer_cards.append(self.hits())
-            return "hits"
+        s = sum(self.player_cards)
+        while (s<20):
+            self.player_cards.append(self.hits())
+            if self.player_cards.count(1) == 1:
+                self.check_usable()
+            s = sum(self.player_cards)
+            if s>21: return "busted"
+        return "sticks"
     
-    def game(self):
-       pass
+    def check_usable(self):
+        i = self.player_cards.index(1,0,2) 
+        self.player_cards[i] = 11
+        if sum(self.player_cards) >21:
+            self.player_cards[i] = 1
+            
 
+    def game(self): #one game is one episode
+        # deal two cards to each
+        self.dealer_cards.append(self.hits())
+        self.dealer_cards.append(self.hits())
+        self.player_cards.append(self.hits())
+        self.player_cards.append(self.hits())
+
+        if self.player_cards.count(1) == 1:
+            self.check_usable()
+            
+        if sum(self.player_cards) == 21: #natural
+            if sum(self.dealer_cards) == 21:
+                reward = 0 
+                return reward
+            else:
+                reward = 1 #player won
+                return reward
+
+        #keep going if no results
+        result = self.agent()
+        if result == "sticks":
+            pass
+        elif result == "busted": #player lost
+            reward = -1
+            return reward
+        
+        result = self.dealer()
+        if result == "sticks":
+            pass
+        elif result == "busted": #player won
+            reward = 1
+            return reward
+        
+        if sum(self.player_cards)>sum(self.dealer_cards):
+            reward = 1
+        elif sum(self.player_cards)<sum(self.dealer_cards):
+            reward = -1
+        else: reward = 0 #tie
+
+        return reward
 
     def render(self):
         pass
