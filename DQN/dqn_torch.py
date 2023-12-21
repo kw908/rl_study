84d39e95fa96853b8160be768978a8306cdbf74b
Kai Wang <20kw16@queensu.ca>
2023-12-21 02:31:26 -0500

    fixed bugs and added some comments

--- DQN/dqn_torch.py
+++ DQN/dqn_torch.py
@@ -8,19 +8,19 @@ class DeepQNetwork(nn.Module):
     def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
         super(DeepQNetwork, self).__init__()
         self.input_dims = input_dims
-        self.fc1_dims = fc1_dims #fc stands for fully connected layer
+        self.fc1_dims = fc1_dims #fc stands for fully connected layer, also known as "linear layer"
         self.fc2_dims = fc2_dims
         self.n_actions = n_actions
         self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims) # aristerisk means any number of dimensions
         self.fc2 = nn.Linear(self.fc1_dims,self.fc2_dims)
         self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
         self.optimizer = optim.Adam(self.parameters(),lr=lr)
-        self.loss = nn.MSELoss()
+        self.loss = nn.MSELoss() # as in the DQN paper
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-    def forward(self, state):
-        x = F.relu(self.fc1(state))
+    def forward(self, state): # the network
+        x = F.relu(self.fc1(state))  
         x - F.relu(self.fc2(x))
         actions = self.fc3(x)
 
@@ -50,6 +50,8 @@ class Agent():
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool_)
 
     def store_transition(self, state, action, reward, state_, done):
+        # store the experience replay D
+        # "state" is current state, "state_" is the next state
         index = self.meme_cntr 
         self.state_memory[index] = state
         self.new_state_memory[index] = state_
@@ -67,31 +69,39 @@ class Agent():
         else:
             action = np.random.choice(self.action_space)
 
-            return action
+        return action
         
     def learn(self):
-        if self.mem_cntr < self.batch_size:
+        if self.meme_cntr < self.batch_size: #start learning only when there is enough samples in the batch
             return
         
         self.Q_eval.optimizer.zero_grad()
 
-        max_mem = min(self.mem_cntr, self.mem_size)
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)
+        max_mem = min(self.meme_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, self.batch_size, replace=False)  # randomly sample a minibatch of size "batch_size", "batch" here means minibatch
 
         batch_index = np.arange(self.batch_size, dtype=np.int32)
-
+        
+        # Convert the batches to tensor
         state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
         new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
-        reward_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
+        reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
         terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
+
+        """indexing with array is handy here because the minibatch is randomly picked from the whole batch"""
 
         action_batch = self.action_memory[batch]
 
-        q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
-        q_next = self.Q_eval.forward(new_state_batch)
+        q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch] # q value of each action in the batch
+
+        #output of Q_eval.forward is 64x4, q_eval is (64,)
+
+        q_next = self.Q_eval.forward(new_state_batch) # why not [batch_index, action_batch] again
         q_next[terminal_batch] = 0.0
 
-        q_target = reward_batch + self.gamma * T.max(q_next, dim=1)[0]
+        q_target = reward_batch + self.gamma * T.max(q_next, dim=1)[0] # q_next is 64x4, dim=1 means taking the maximum along the action axis
+
+        """tensor uses both positive and negative indices to index the dimensions of array, works the same way as ndarray indexing"""
 
         loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
         loss.backward()
